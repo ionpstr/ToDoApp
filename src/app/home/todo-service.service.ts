df7d9ef63dd7ce73244f97b03fe456401e6fa4c8
CEDMD\crme010 <ion.postoronca@iongroup.com>
2023-03-10 16:03:16 +0200

    added deleted tasks functionality

--- src/app/home/todo-service.service.ts
+++ src/app/home/todo-service.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { LoggerService } from '../login/logger.service';
 import { Observable, Subject, BehaviorSubject } from 'rxjs';
 import { Item } from '../models/item.model';
+
 @Injectable()
 export class TodoServiceService {
   private dataStore: Item[] = [];
-
+  private deletedTasks: Item[] = [];
   constructor(private logger: LoggerService) {
     this.logger.userIsAtuth().subscribe((user) => {
       if (user.username == null) {
-        this.itemsSubject.complete();
+        this.dataStore = [];
+        this.itemsSubject.next(this.dataStore);
       }
     });
     this.items = this.itemsSubject.asObservable();
@@ -44,14 +46,36 @@ export class TodoServiceService {
   }
 
   updateTask(item: Item): void {
-    this.dataStore = this.dataStore.map((i) => (item === i ? item : i));
+    this.dataStore = this.dataStore.filter((i) => i != item);
+    this.dataStore = [...this.dataStore, item];
     localStorage.setItem('items', JSON.stringify(this.dataStore));
     this.itemsSubject.next(this.dataStore);
   }
 
   deleteTask(item: Item): void {
+    this.deletedTasks = [...this.deletedTasks, item];
+    localStorage.setItem('deletedItems', JSON.stringify(this.deletedTasks));
     this.dataStore = this.dataStore.filter((i) => i !== item);
     localStorage.setItem('items', JSON.stringify(this.dataStore));
+    this.itemsSubject.next(this.dataStore);
+  }
+  getDeletedTasks() {
+    const date = new Date();
+    this.deletedTasks = [
+      ...this.deletedTasks,
+      ...(JSON.parse(localStorage.getItem('items') as string) || []),
+    ];
+    this.deletedTasks = this.deletedTasks.filter((item) => {
+      if (item.createdAt) {
+        let d = new Date(item.createdAt);
+        return date.getTime() - d.getTime() <= 2628000000;
+      } else {
+        return false;
+      }
+    });
+    this.dataStore = [...this.dataStore, ...this.deletedTasks];
+    this.deletedTasks = [];
+    localStorage.removeItem('deletedItems');
     this.itemsSubject.next(this.dataStore);
   }
 }
